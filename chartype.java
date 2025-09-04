8249b39dfa592d618edfbbeb868eae31c542d50a
Manvendra Singh <manvendrasingh2260@gmail.com>
2025-09-04 20:26:38 +0530

    Add files via upload

---
+++ chartype.java
@@ -0,0 +1,92 @@
+public class chartype {
+    public static void main(String[] args) {
+        // character to integer convert
+
+        char n = 'a';
+        System.out.println(n);
+        // impilcit typeccasting
+        char ch = 'A';
+        int x = ch;
+        System.out.println(x);
+        // explicit typecasting
+        char m = 'a';
+        int y = (int)m;
+        System.err.println(y);
+        // 3rd method
+        char i = 'b';
+        System.out.println((int)i);
+
+        // 4th method
+        char j = 'a';
+        System.out.println(j+0);
+
+        // 5th method
+        char k = 'a';
+        System.out.println(k+k);
+
+        // integer to character convert
+        int p = 98;
+        char mh = (char)p;
+        int q = 50; 
+        int r = 32;
+        System.out.println(mh);
+        System.out.println((char)q);
+        System.out.println((char)r);
+
+        
+
+        
+    }
+    
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
