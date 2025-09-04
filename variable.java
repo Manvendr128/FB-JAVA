854dddd45e2ad95752a0fbb4a8d785c51d046ae8
Manvendra Singh <manvendrasingh2260@gmail.com>
2025-09-04 20:26:05 +0530

    Add files via upload

---
+++ variable.java
@@ -0,0 +1,59 @@
+// import java.util.*;
+public class variable {
+    public static void main(String[] args) {
+        // Scanner sc = new Scanner(System.in);
+        // // String name = sc.next();
+        // // System.out.println(name);
+        // System.out.print("enter fisrt num");
+        // int c = sc.nextInt();
+
+        // System.out.print("enter second num");  
+        // int d = sc.nextInt();
+
+        // int add = c + d;
+        // System.out.println(add);
+
+        int a = 10;
+        int b =  5;
+        int sum = a + b;
+        System.out.println(sum);
+        int ans = a * b / a - b;
+        int answ = (a * b ) / (a - b);
+        System.out.println(ans);        
+        System.out.println(answ);
+        
+        int x = 7;
+        System.out.println(x);
+        x = x + 2;
+        System.out.println(x);
+
+    }
+    
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
